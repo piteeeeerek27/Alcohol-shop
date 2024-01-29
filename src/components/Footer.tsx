e3aa4483fb81616ac1ed65c0fb15261417373429
Piotr <rzesnyppiotrr@gmail.com>
2024-01-29 17:33:26 +0100

    footer direction links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,15 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import EmailIcon from "@mui/icons-material/Email";
 import CallIcon from "@mui/icons-material/Call";
+import { useNavigate } from "react-router-dom";
 
 const Footer: React.FC = () => {
+	const navigate = useNavigate();
+
+	const handleIconNavigate = (url: string) => {
+		navigate(url);
+	};
+
 	return (
 		<footer className="footer">
 			<div className="footer-info">
@@ -15,24 +22,27 @@ const Footer: React.FC = () => {
 			</div>
 
 			<div className="footer-links">
-				<a className="email-icon" href="">
+				<a
+					className="email-icon"
+					href="#"
+					onClick={() => handleIconNavigate("/email")}>
 					<EmailIcon />
 				</a>
-				<a className="phone-icon" href="">
+				<a className="phone-icon" href="#">
 					<CallIcon />
 				</a>
 				<a
 					className="ig-icon"
 					href="https://www.instagram.com/"
 					target="_blank"
-					rel="noopener noreferrer">
+					onClick={() => handleIconNavigate("/instagram")}>
 					<InstagramIcon />
 				</a>
 				<a
 					className="fb-icon"
 					href="https://www.facebook.com/"
 					target="_blank"
-					rel="noopener noreferrer">
+					onClick={() => handleIconNavigate("/facebook")}>
 					<FacebookIcon />
 				</a>
 			</div>
